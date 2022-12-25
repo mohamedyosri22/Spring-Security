0187b403f139e609a72bdb7603d684bfd5f6af33
mhmdyouali <mhmd.you@yahoo.com>
2022-12-25 18:03:25 +0200

    assign each user with role and page

--- src/main/java/com/spring/securityconfig/SecurityConfiguration.java
+++ src/main/java/com/spring/securityconfig/SecurityConfiguration.java
@@ -3,10 +3,22 @@ package com.spring.securityconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.MatcherType.ant;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 
 @Configuration
@@ -14,13 +26,71 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class SecurityConfiguration {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("USER201")
-                .roles("USER")
+    public UserDetailsService userDetailsService() throws Exception{
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(
+                User.withUsername("anakin")
+                        .password(encoder().encode("anakin201"))
+                        .roles("ADMIN")
+                        .build()
+        );
+
+        manager.createUser(
+                User.withUsername("user")
+                        .password(encoder().encode("user201"))
+                        .roles("USER")
+                        .build()
+        );
+
+        manager.createUser(
+                User.withUsername("manager")
+                        .password(encoder().encode("manager201"))
+                        .roles("MANGER")
+                        .build()
+        );
+
+        return manager;
+    }
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authCustomizer -> authCustomizer
+                                .requestMatchers("/api/main").permitAll()
+                                .requestMatchers("/api/profile").authenticated()
+                                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/api/manage").hasAnyRole("ADMIN","MANGER")
+                                .anyRequest().permitAll()
+
+                        //.requestMatchers("/api/main").permitAll()
+                        //.requestMatchers("/api/profile").authenticated()
+                )
                 .build();
-        return new InMemoryUserDetailsManager(user);
+
+    }
+
+
+    @Bean
+    public static PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
+
+    /*@Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }*/
